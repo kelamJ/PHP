30acc8cd8e194dbc996f4f225558e50a4b8ec164
kelamJ <malekjulien80@gmail.com>
2022-12-21 16:02:46 +0100

    update

---
+++ Boucles/exercice3.php
@@ -0,0 +1,17 @@
+<?php
+function table($nbr, $nbr2)
+        {
+            $table = '<table border="1">';
+            for ($a=0; $a <= $nbr; $a++) {
+                $table .= '<tr>';
+                for ($b=-0; $b <= $nbr2 ; $b++) {
+                    $table .= '<td>'.$a*$b.'</td>';
+                }
+                $table .= '</tr>';
+            }
+            $table .= '</table>';
+            return $table;
+        }
+
+        echo table(12, 12);
+?>
