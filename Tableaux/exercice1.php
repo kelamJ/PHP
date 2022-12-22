c79acbfa4fbc4c95af28ea58575beb98f42490c5
kelamJ <malekjulien80@gmail.com>
2022-12-22 16:53:14 +0100

    update

---
+++ Tableaux/exercice1.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table border="2">
+<?php 
+$tableau = array(
+    "Janvier"   =>  31,
+    "Février"   =>  28,
+    "Mars"  =>  31,
+    "Avril"  =>  30,
+    "Mai"  =>  31,
+    "Juin"  =>  30,
+    "Juillet"  =>  31,
+    "Aout"  =>  31,
+    "Septembre"  =>  30,
+    "Octrobre"  =>  31,
+    "Novembre"  =>  30,
+    "Décembre"  =>  31,
+); 
+?>
+        <?php for($row=0;$row<=12;$row++)
+        { ?>
+        <tr>
+        <?php for($col=0;$col<=12;$col++)
+        {
+            $result=$row*$col;
+            ?>
+        <td><?php echo $result ?></td>
+        <?php } ?>
+        </tr>
+        <?php } ?>
+        
+    </table>
+</body>
+</html>
